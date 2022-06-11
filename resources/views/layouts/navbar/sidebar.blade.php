7cbcb10c92f0af135d6820f9d830ea5c8cbf6c0c
iampapaisarkar <iampapaisarkar@gmail.com>
2022-06-11 20:55:22 +0530

    logo fix

--- resources/views/layouts/navbar/sidebar.blade.php
+++ resources/views/layouts/navbar/sidebar.blade.php
@@ -2,7 +2,7 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('dashboard')}}">
-        <div class="sidebar-brand-icon rotate-n-15">
+        <div class="sidebar-brand-icon">
             <img src="{{asset('images/logo.png')}}" class="w-100"/>
         </div>
         <div class="sidebar-brand-text mx-3">HOTELS <sup>Portal</sup></div>
